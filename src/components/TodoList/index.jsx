d4058085e4ba1660014e84e91cb5cde3e2c0f055
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-09-08 20:36:05 -0300

    Finalização do Projeto

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,38 +1,13 @@
 import TodoItem from "../TodoItem"
-import { useState } from 'react';
 import styles from "./todoList.module.css"
 
-export default function TodoList() {
-
-    const [taskList, setTaskList] = useState([])
-    const [newTask, setNewTask] = useState("")
-
-    function addTask(event) {
-        event.preventDefault()
-        setTaskList([...taskList, newTask]);
-        setNewTask("");
-    }
+export default function TodoList(props) {
 
     return (
-        <div>
-            <h1>
-                To Do List
-            </h1>
-
-            <form onSubmit={(event) => addTask(event)}>
-                <input
-                    placeholder="Tarefa"
-                    value={newTask}
-                    onChange={(event) => setNewTask(event.target.value)} />
-                <button type="submit">Salvar</button>
-            </form>
-
             <ul>
-                {taskList.map((task, index) => (
+                {props.tasks.map((task, index) => (
                     <TodoItem key={index} task={task} />
                 ))}
             </ul>
-
-        </div>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,33 @@
 import TodoList from "../../components/TodoList"
+import { useState } from 'react';
 import styles from "./home.module.css"
 
 export default function Home() {
+
+    const [taskList, setTaskList] = useState([])
+    const [newTask, setNewTask] = useState("")
+
+    function addTask(event) {
+        event.preventDefault()
+        setTaskList([...taskList, newTask]);
+        setNewTask("");
+    }
+
     return (
         <div>
-            <TodoList />
+            <h1>
+                To Do List
+            </h1>
+
+            <form onSubmit={(event) => addTask(event)}>
+                <input
+                    placeholder="Tarefa"
+                    value={newTask}
+                    onChange={(event) => setNewTask(event.target.value)} />
+                <button type="submit">Salvar</button>
+            </form>
+
+            <TodoList tasks={taskList} />
         </div>
     )
 }
